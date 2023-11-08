d8da9b7dbe39dd48391900da37f8489be03e0792
Boris Blokland <borisblokland@gmail.com>
2023-11-08 20:32:15 +0100

    WIP parsing incomplete solution

--- sudoku_solver/sudoku_solver.py
+++ sudoku_solver/sudoku_solver.py
@@ -1,35 +1,65 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Cell:
     value: int = 0
 
+
 @dataclass
 class Row:
-    cells: list[Cell] = None
+    def __init__(self):
+        self.cells = []
 
 
 class Sudoku:
     def __init__(self):
         self.rows = []
-    
-    def from_string(self, string_sudoku):
-        lines = string_sudoku.splitlines()
-        for idx,line in enumerate(lines):
-            if idx in [3,7]:
+
+    @staticmethod
+    def from_string(string_sudoku: str) -> 'Sudoku':
+        sudoku = Sudoku()
+        string_sudoku_stripped = string_sudoku.strip().strip("\n")
+        lines = string_sudoku_stripped.splitlines()
+        for idx, line in enumerate(lines):
+            if idx in [3, 7]:
                 continue
-            line = line.replace("|", "")
-            row = Row()
-            for number in line:
-                cell = Cell(int(number))
-                row.cells.append(cell)
-            self.rows.append(row)
-                
-        
+            row = Sudoku.parse_sudoku_row_string(line)
+            sudoku.rows.append(row)
+        return sudoku
+    
+    @staticmethod
+    def parse_sudoku_row_string(string_sudoku_row: str) -> Row:
+        row = Row()
+        number_indices = [0, 2, 4, 8, 10, 12, 16, 18, 20]
+        for index in number_indices:
+            number = string_sudoku_row[index]
+            if number == " ":
+                value = 0
+            else:
+                value = int(number)
+            cell = Cell(value)
+            row.cells.append(cell)
+        return row
+
+    def to_string(self) -> str:
+        string_sudoku = ""
+        for idx, row in enumerate(self.rows):
+            for cell_number, cell in enumerate(row.cells):
+                string_sudoku += str(cell.value) + " "
+                if cell_number == 2 or cell_number == 5:
+                    string_sudoku += "| "
+            string_sudoku = string_sudoku.strip()
+            string_sudoku += "\n"
+            if idx == 2 or idx == 5:
+                string_sudoku += "---------------------\n"
+        return string_sudoku
 
 
 def solve_sudoku(string_sudoku):
     sudoku = Sudoku()
+    print(string_sudoku)
     sudoku.from_string(string_sudoku)
-    return string_sudoku
-
+    result = sudoku.to_string()
+    print(result)
+    return result
