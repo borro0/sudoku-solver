d70910871ea8a93a2ba87248ad3611e76cecf88f
Boris Blokland <bblokland@lely.com>
2023-11-13 19:17:31 +0100

    unified parsing, added Cluster base class

--- sudoku_solver/sudoku_solver.py
+++ sudoku_solver/sudoku_solver.py
@@ -9,13 +9,9 @@ class Cell:
     def is_solved(self) -> bool:
         return self.value != 0
 
-class Column:
+class Cluster:
     def __init__(self):
         self.cells: List[Cell] = []
-
-    def add_cell(self, value: int):
-        cell = Cell(value)
-        self.cells.append(cell)
 
     def solve(self):
         possible_values = set(range(1, 10)) - set([cell.value for cell in self.cells])
@@ -24,20 +20,11 @@ class Column:
                 if len(possible_values) == 1:
                     cell.value = possible_values.pop()
 
-class Row:
-    def __init__(self):
-        self.cells: List[Cell] = []
+class Column(Cluster):
+    pass
 
-    def add_cell(self, value: int):
-        cell = Cell(value)
-        self.cells.append(cell)
-
-    def solve(self):
-        possible_values = set(range(1, 10)) - set([cell.value for cell in self.cells])
-        for cell in self.cells:
-            if not cell.is_solved():
-                if len(possible_values) == 1:
-                    cell.value = possible_values.pop()
+class Row(Cluster):
+    pass
 
 
 class Sudoku:
@@ -45,6 +32,7 @@ class Sudoku:
         self.rows: List[Row] = []
         self.columns: List[Column] = []
         for _ in range(9):
+            self.rows.append(Row())
             self.columns.append(Column())
 
     def solve(self) -> None:
--- sudoku_solver/sudoku_string_parser.py
+++ sudoku_solver/sudoku_string_parser.py
@@ -4,20 +4,19 @@ def parse_sudoku_from_string(string_sudoku: str) -> "Sudoku":
     sudoku = Sudoku()
     string_sudoku_stripped = string_sudoku.strip().strip("\n")
     lines = string_sudoku_stripped.splitlines()
+    row_number = 0
     for idx, line in enumerate(lines):
         if idx in [3, 7]:
             continue
-        row = parse_sudoku_row_string(sudoku, line)
-        sudoku.rows.append(row)
+        parse_sudoku_row_string(sudoku, line, row_number)
+        row_number += 1
     return sudoku
 
-def parse_sudoku_row_string(sudoku: Sudoku, string_sudoku_row: str) -> Row:
-    row = Row()
+def parse_sudoku_row_string(sudoku: Sudoku, string_sudoku_row: str, row_number: int) -> Row:
     number_indices = [0, 2, 4, 8, 10, 12, 16, 18, 20]
     for idx, index in enumerate(number_indices):
         number = string_sudoku_row[index]
         value = int(number) if number != " " else 0
         cell = Cell(value)
-        row.cells.append(cell)
+        sudoku.rows[row_number].cells.append(cell)
         sudoku.columns[idx].cells.append(cell)
-    return row
